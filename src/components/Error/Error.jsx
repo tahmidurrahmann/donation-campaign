a1b1d87e6b1f2c8dd6f2069b453196d60e687a8e
tahmidurrahmann <mirazurrahmannn@gmail.com>
2023-09-26 02:26:57 +0600

    added the errorElement, pieChart

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,7 +1,20 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+    const error = useRouteError();
+    console.log(error);
     return (
-        <div>
-            <h1>opps!</h1>
+        <div className="flex justify-center items-center h-[100vh]">
+            <div className="text-center">
+                <p className="text-xl">
+                    <i>{error.statusText || error.message}</i>
+                </p>
+                {
+                    error.status === 404 && <div>
+                         <h1 className="text-xl">Page Not Found!</h1>
+                    </div>
+                }
+            </div>
         </div>
     );
 };
